5335147e144edf06f91cb144b0abada6eae201c7
alexmanul <aleksejs.marcena@gmail.com>
2020-01-18 23:29:56 +0200

    New screen elements

--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.androidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getStatusBarHeight();
+    }
+
+    private int getStatusBarHeight() {
+        int height;
+        Resources myResources = getResources();
+        int idStatusBarHeight = myResources.getIdentifier( "status_bar_height", "dimen", "android");
+        if (idStatusBarHeight > 0) {
+            height = getResources().getDimensionPixelSize(idStatusBarHeight);
+            Toast.makeText(this, "Status Bar Height = " + height, Toast.LENGTH_LONG).show();
+        } else {
+            height = 0;
+            Toast.makeText(this, "Resources NOT found", Toast.LENGTH_LONG).show();
+        }
+        return height;
     }
 }
